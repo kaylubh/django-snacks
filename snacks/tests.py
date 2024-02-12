5da2d2696d2c471b064f2f84ff49ab25a43ce80a
Caleb Hemphill <calebhemp@gmail.com>
2024-02-12 18:33:38 -0500

    Add unit tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class SnacksTests(SimpleTestCase):
+
+    # home page
+    def test_home_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    # about page
+    def test_about_status_code(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_template(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertTemplateUsed(response, 'base.html')
